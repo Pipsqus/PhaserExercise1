9a6c948466d4273b86e1e2c57dc202fae4d94b3c
Pipsqus <andreaforconi@protonmail.com>
2021-03-09 11:06:09 +0100

    restructured the folders

--- Current/src/mainScene.js
+++ Current/src/mainScene.js
@@ -3,7 +3,20 @@ function init() {
 	this.enemyMaxY = 500;    
 }
 
+// ------------------------------------------------
+function preload ()
+{
+	this.load.spritesheet('paladin', 'assets/Paladin.png', {frameWidth: 16, frameHeight: 16, endFrame: 2})
+	this.load.spritesheet('tool', 'assets/Tool.png', {frameWidth: 16, frameHeight: 16, endFrame: 22});
+	this.load.spritesheet('player', 'assets/Player.png', {frameWidth: 16, frameHeight: 16});
+	this.load.spritesheet('tileSet', 'assets/Tile.png', {frameWidth: 16, frameHeight: 16});
+	this.load.spritesheet('bomb', 'assets/Explosion.png', {frameWidth: 96, frameHeight: 96});
+	this.load.spritesheet('spells', 'assets/Spells.png', {frameWidth: 16, frameHeight: 16});
+	this.load.spritesheet('shortWeapon', 'assets/ShortWeapon.png', {frameWidth: 16, frameHeight: 16})
+	this.load.image('background', 'assets/background.png');
+}
 
+// ------------------------------------------------
 function create()
 	{
 		var background = this.add.image(0, 0, 'background')
@@ -26,8 +39,8 @@ function create()
 		});
 
 
-		this.someTool = this.add.sprite(700, 300, 'tool', 15);
-		this.someTool.setScale(3);
+		this.magicalLadel = this.add.sprite(700, 300, 'tool', 15);
+		this.magicalLadel.setScale(3);
 
 		this.player = this.add.sprite(100, 300, 'paladin', 0);
 		this.player.setScale(3.3);
@@ -43,34 +56,32 @@ function create()
 
 		let enemyEntities = this.enemies.getChildren();
 		for (var i = 0; i < enemyEntities.length; i++) {
-			enemyEntities[i].speed = Math.random() * 3;
-			if (enemyEntities[i].speed < 2) {
-					enemyEntities[i].speed += 3;
-			}
+			var randomSpeed = Math.random() * 3;
+			enemyEntities[i].speed = randomSpeed > 2 ? randomSpeed : randomSpeed + 3;
 		}
 
 		this.bomb = this.add.sprite(-100, -300, 'bomb', 0)
 		this.bomb.setScale(2)
-
+		
+		this.spell = this.add.sprite(1000, 1000, 'shortWeapon', 9).setScale(2);
+		var gameInstance = this
 		this.input.keyboard.on('keydown-ESC', function(){
-			game.scene.start('uiScene');
-		} )
-
+			gameInstance.spell.x = gameInstance.player.x;
+			gameInstance.spell.y = gameInstance.player.y;
+			
+			gameInstance.time.delayedCall(450, function() {
+				gameInstance.spell.x = 1000;
+				gameInstance.spell.y = 1000;
+				}, [], this);
+			
+		})
 	}
 	
-function preload ()
-{
-	this.load.spritesheet('paladin', 'assets/Paladin.png', {frameWidth: 16, frameHeight: 16, endFrame: 2})
-	this.load.spritesheet('tool', 'assets/Tool.png', {frameWidth: 16, frameHeight: 16, endFrame: 22});
-	this.load.spritesheet('player', 'assets/Player.png', {frameWidth: 16, frameHeight: 16});
-	this.load.spritesheet('tileSet', 'assets/Tile.png', {frameWidth: 16, frameHeight: 16});
-	this.load.spritesheet('bomb', 'assets/Explosion.png', {frameWidth: 96, frameHeight: 96});
-	this.load.image('background', 'assets/background.png');
-}
-
+// ------------------------------------------------------
 function update()
 {
-	if (!this.player.isAlive) {return}
+	if (!this.player.isAlive) {return};
+	
 	if (this.input.activePointer.leftButtonDown()) {
 	this.player.x += 4;
 	if (!hasAnimationStarted) {
@@ -83,11 +94,10 @@ function update()
 		hasAnimationStarted = false;
 	}
 
-let enemyEntities = this.enemies.getChildren();
+	let enemyEntities = this.enemies.getChildren();
 
 	for (var i = 0; i < enemyEntities.length; i++) {
-		if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), enemyEntities[i].getBounds()))
-	{
+		if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), enemyEntities[i].getBounds())) {
 		this.player.isAlive = false;
 		this.bomb.x = this.player.x;
 		this.bomb.y = this.player.y;
@@ -95,29 +105,60 @@ let enemyEntities = this.enemies.getChildren();
 			key: 'explosion',
 			frames: 'bomb',
 			frameRate: 15,
-			repeat: 0 
+			repeat: -1 
 		};
 		var explosion = this.anims.create(explosionAnimation);
 		this.bomb.play(explosion);
 		// this.cameras.main.shake(500);
 		this.time.delayedCall(250, function() {this.cameras.main.fade(250)}, [], this);
 		this.time.delayedCall(500, function() {this.scene.restart()}, [], this)
+		}
+		if (enemyEntities[i].y > this.enemyMaxY) 
+		{
+			enemyEntities[i].speed *= -1;
+		} else if (enemyEntities[i].y < this.enemyMinY) 
+		{
+			enemyEntities[i].speed *= -1;	
+		}
+		enemyEntities[i].y += enemyEntities[i].speed
 	}
-	if (enemyEntities[i].y > this.enemyMaxY) 
-	{
-		enemyEntities[i].speed *= -1;
-	} else if (enemyEntities[i].y < this.enemyMinY) 
-	{
-		enemyEntities[i].speed *= -1;	
-	}
-	enemyEntities[i].y += enemyEntities[i].speed
-}
 
-if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), this.someTool.getBounds())) 
+	if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), this.magicalLadel.getBounds())) 
 	{
-			this.player.isAlive = false;
-			this.time.delayedCall(250, function() {this.cameras.main.fade(250)}, [], this);
-			this.time.delayedCall(500, function() {this.scene.restart()}, [], this);
+		this.player.isAlive = false;
+		this.time.delayedCall(250, function() {this.cameras.main.fade(250)}, [], this);
+		this.time.delayedCall(500, function() {this.scene.restart()}, [], this);
+	}
+	
+	if (this.spell) {
+		this.spell.x += 7;
+		
+	const explosionAnimation = {
+			key: 'explosion',
+			frames: 'bomb',
+			frameRate: 15,
+			repeat: 0 
+			};
+			
+	var explosion = this.anims.create(explosionAnimation);
+		
+	var gameInstance = this;
+	for (var i = 0; i < enemyEntities.length; i++) {
+		if (Phaser.Geom.Intersects.RectangleToRectangle
+		(enemyEntities[i].getBounds(), this.spell.getBounds())) {
+			enemyEntities[i].destroy();
+			this.bomb.x = this.spell.x;
+			this.bomb.y = this.spell.y;
+			this.bomb.play(explosion);
+			gameInstance.spell.x = 1000;
+			gameInstance.spell.y = 1000;
+			
+			gameInstance.time.delayedCall(800, function() {
+				gameInstance.bomb.x = 1000;
+				gameInstance.bomb.y = 1000;
+				}, [], this);
+			}
+		}		
 	}
 };
 
--- Current/src/uiScene.js
+++ Current/src/uiScene.js
@@ -1,41 +1,17 @@
-const explosionAnimation = {
-	key: 'explosion',
-	frames: 'bomb',
-	frameRate: 15,
-	repeat: 0 
-};
-
 class uiScene extends Phaser.Scene {
-constructor(mainSceneConfig) {
-		super(mainSceneConfig)
-}
-
-preload() {
-		this.load.spritesheet('spells', 'assets/Spells.png', {frameWidth: 16, frameHeight: 16});
-}
-
-create() {
-	this.spell = this.add.sprite(game.scene.getAt(0).player.x, game.scene.getAt(0).player.y, 'spells', 3);
-	this.input.keyboard.on('keydown-SPACE', function(){})
-	
-}
-
-update() {
-	if (this.spell) {
-		this.spell.x += 4;
-		
-			let enemyEntities = game.scene.getAt(0).enemies.getChildren();
-	for (var i = 0; i < enemyEntities.length; i++) {
-		if (Phaser.Geom.Intersects.RectangleToRectangle
-		(enemyEntities[i].getBounds(), this.spell.getBounds())) {
-			enemyEntities[i].destroy();
-			game.scene.getAt(0).bomb.x = this.spell.x;
-			game.scene.getAt(0).bomb.y = this.spell.y;
-			var explosion = this.anims.create(explosionAnimation);
-			this.spell.destroy();
-			}
-		}		
+	constructor(mainSceneConfig) {
+			super(mainSceneConfig)
 	}
-	this.time.delayedCall(350, function() {this.spell.destroy()}, [], this);
+
+	preload() {
+			
+	}
+
+	create() {
+
+	}
+
+	update() {
+
 	}
 }
